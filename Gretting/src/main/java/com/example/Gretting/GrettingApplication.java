f833acab3dd2a753043021112f687c2f0587d076
Giovanny Ospina Alzate <giospin@bancolombia.com.co>
2024-10-22 17:40:57 -0500

    se sube el micro de Greetings

---
+++ Gretting/src/main/java/com/example/Gretting/GrettingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gretting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrettingApplication.class, args);
+	}
+
+}
---
+++ Gretting/src/test/java/com/example/Gretting/GrettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gretting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
